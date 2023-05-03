5647605f0557cab74f2c21057ea36c3f46840009
Lsig <logis21@ru.is>
2023-05-03 16:12:33 +0200

    Get todos by project_id, endpoint created

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -8,12 +8,12 @@ use crate::{
 use actix_web::{delete, get, patch, post, web, HttpResponse, Responder};
 use serde_json::json;
 
-#[get("/api/v1/projects/{user_id}")]
-pub async fn user_todos(path: web::Path<i32>, data: web::Data<DbPool>) -> impl Responder {
+#[get("/api/v1/users/{user_id}/projects")]
+pub async fn user_projects(path: web::Path<i32>, data: web::Data<DbPool>) -> impl Responder {
     let id = path.into_inner();
     let user_projects = match sqlx::query_as!(ProjectModel, 
         "
-        SELECT p.project_id, p.name
+        SELECT p.project_id, p.project_name
         FROM projects p
         INNER JOIN users u on p.user_id = u.user_id
         WHERE u.user_id = $1 
@@ -28,7 +28,32 @@ pub async fn user_todos(path: web::Path<i32>, data: web::Data<DbPool>) -> impl R
             },
         };
 
-    let json = json!(&user_projects);
+    let res = json!(&user_projects);
 
-    return HttpResponse::Ok().json(json);
+    return HttpResponse::Ok().json(res);
+}
+
+#[get("/api/v1/projects/{project_id}/todos")]
+pub async fn project_todos(path: web::Path<i32>, data: web::Data<DbPool>) -> impl Responder {
+    let id = path.into_inner();
+    let project_todos = match sqlx::query_as!(TodoModel, 
+        "
+        SELECT *
+        FROM todos t
+        NATURAL JOIN projects p
+        WHERE p.project_id = $1 
+        ", 
+        id)
+        .fetch_all(&data.0)
+        .await {
+            Ok(data) => data, 
+            Err(_) => {
+                eprint!("Database Error!");
+                vec![]
+            },
+        };
+
+    let res = json!(&project_todos);
+
+    return HttpResponse::Ok().json(res);
 }
