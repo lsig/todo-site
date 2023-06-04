eccba0d6d0fa1a6e8a3b23485cb9bdf47d4303a5
Lsig <logis21@ru.is>
2023-06-04 13:46:47 +0000

    Frontend reconfigured for React and Tailwind

--- frontend/src/app.js
+++
@@ -1,9 +0,0 @@
-import { todoNote, projects } from "./todo";
-
-console.log("this is connected");
-
-const newTodo = todoNote("Hello", "test", 3, new Date(2023, 9, 11));
-const newTodo1 = todoNote("Hello", "test", 3, new Date(2023, 9, 11));
-
-console.log(newTodo);
-console.log(newTodo1);
--- frontend/src/container.js
+++
@@ -1,8 +0,0 @@
-import { todoNote, project } from "./todo";
-
-const createSmallTodoNote = (todo) => {
-  const todoDiv = document.createElement("div");
-  const todoTitle = document.createElement("div");
-  const todoPrior = document.createElement("div");
-  const todoCompletedBtn = document.createElement("button");
-};
