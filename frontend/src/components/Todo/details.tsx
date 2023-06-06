61cd238c3b5f248804d5ef531a87da89cfff1626
Lsig <logis21@ru.is>
2023-06-06 23:10:26 +0000

    Detail component inside todo

---
+++ frontend/src/components/Todo/details.tsx
@@ -0,0 +1,11 @@
+export function DetailsBtn() {
+  return (
+    <button
+      className={
+        "rounded-lg w-28 h-10 bg-purple-300 text-purple-400 disabled:opacity-100"
+      }
+    >
+      Details
+    </button>
+  );
+}
