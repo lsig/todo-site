92908ffe4aa3ccb414af3e384915af25d995941b
Lsig <logis21@ru.is>
2023-06-08 17:12:45 +0000

    Sidebar and overview design almost completes, needs functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,13 +9,17 @@ import { Todo } from "./components/Todo/todo";
 import { NewTaskBtn } from "./components/ProjectOverview/NewTask";
 import { ProjectHeader } from "./components/ProjectOverview/header";
 import { ProjectOverview } from "./components/ProjectOverview/projectOverview";
+import { NewProjectBtn } from "./components/Sidebar/newProject";
+import { Project } from "./components/Sidebar/project";
+import { SidebarHeader } from "./components/Sidebar/sidebarHeader";
+import { Sidebar } from "./components/Sidebar/sidebar";
 
 function App() {
   return (
-    <>
-      <br />
+    <div className="flex">
+      <Sidebar />
       <ProjectOverview />
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/ProjectOverview/projectOverview.tsx
+++ frontend/src/components/ProjectOverview/projectOverview.tsx
@@ -4,7 +4,7 @@ import { ProjectHeader } from "./header";
 export function ProjectOverview() {
   // TODO: Need to add Todos automatically
   return (
-    <div className="flex flex-col h-5/6 w-3/4 p-5 bg-gray-600 rounded-3xl sm:min-h-[555px] md:min-h-[655px] lg:min-h-[700px]">
+    <div className="flex flex-col h-5/6 w-3/4 p-5 m-4 bg-gray-600 rounded-3xl sm:min-h-[555px] md:min-h-[655px] lg:min-h-[700px]">
       <ProjectHeader />
       <Todo />
       <Todo />
